c6145dedb133ac91338559cad7ce3ae326d1edb6
Vadzim <vadzimk@hotmail.com>
2019-08-26 14:15:20 -0700

    applied connector components in ProductDisplay

--- src/ProductDisplay.js
+++ src/ProductDisplay.js
@@ -4,75 +4,83 @@ import React from 'react';
 import {ProductEditor} from "./ProductEditor";
 import {ProductTable} from "./ProductTable";
 import {connect} from "react-redux";
-import {deleteProduct, saveProduct} from "./store";
+//import {deleteProduct, saveProduct} from "./store";
+import {EditorConnector} from "./store/EditorConnector";
+import T from './store/T';
+import {TableConnector} from "./store/TableConnector";
+import {startCreatingProduct} from "./store/stateActionCreators";
 
+const ConnectedEditor = EditorConnector(T.PRODUCTS, ProductEditor);
+const ConnectedTable = TableConnector(T.PRODUCTS, ProductTable);
 
 class ProductDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showEditor: false,
-            selectedProduct: null,
-        }
-    }
-
-    saveProduct = (product) => {
-        this.props.saveCallback(product);
-        this.setState(
-            {
-                showEditor: false,
-                selectedProduct: null,
-            }
-        );
-    };
-
-    cancelEditing = () => {
-        this.setState(
-            {
-                showEditor: false,
-                selectedProduct: null,
-            }
-        );
-    };
-
-    startEditing = (product) => {
-        this.setState(
-            {
-                showEditor: true,
-                selectedProduct: product,
-            }
-        );
-    };
-
-    createProduct = () => {
-        this.setState(
-            {
-                showEditor: true,
-                selectedProduct: {},
-            }
-        );
-    };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         showEditor: false,
+    //         selectedProduct: null,
+    //     }
+    // }
+    //
+    // saveProduct = (product) => {
+    //     this.props.saveCallback(product);
+    //     this.setState(
+    //         {
+    //             showEditor: false,
+    //             selectedProduct: null,
+    //         }
+    //     );
+    // };
+    //
+    // cancelEditing = () => {
+    //     this.setState(
+    //         {
+    //             showEditor: false,
+    //             selectedProduct: null,
+    //         }
+    //     );
+    // };
+    //
+    // startEditing = (product) => {
+    //     this.setState(
+    //         {
+    //             showEditor: true,
+    //             selectedProduct: product,
+    //         }
+    //     );
+    // };
+    //
+    // createProduct = () => {
+    //     this.setState(
+    //         {
+    //             showEditor: true,
+    //             selectedProduct: {},
+    //         }
+    //     );
+    // };
 
     render() {
 
         if (this.state.showEditor) {
-            return <ProductEditor
-                key={this.state.selectedProduct.id || -1}
-                product={this.state.selectedProduct}
-                saveCallback={this.saveProduct}
-                cancelCallback={this.cancelEditing}
-
-            />
+            return <ConnectedEditor key={this.props.selected.id || -1}/>
+            // return <ProductEditor
+            //     key={this.state.selectedProduct.id || -1}
+            //     product={this.state.selectedProduct}
+            //     saveCallback={this.saveProduct}
+            //     cancelCallback={this.cancelEditing}
+            //
+            // />
         } else {
             return <div className="m-2">
-                <ProductTable
+                <ConnectedTable/>
+                { /* <ProductTable
                     products={this.props.products}
                     editCallback={this.startEditing}
                     deleteCallback={this.props.deleteCallback}
-                />
+                /> */}
                 <div className="text-center">
                     <button className="btn btn-primary m-1"
-                            onClick={this.createProduct}
+                            onClick={this.props.createProduct}
                     >
                         Create Product
                     </button>
@@ -84,12 +92,12 @@ class ProductDisplay extends React.Component {
 
 
 const mapStateToProps = (storeData) => ({
-    products: storeData.products
+    editing: storeData.stateData.editing,
+    selected: storeData.modelData[T.PRODUCTS].find(p => p.id === storeData.stateData.selectedId) || {}
 });
 
-const mapDispatchToProps =  { //object form
-    saveCallback: saveProduct,
-    deleteCallback: deleteProduct,
+const mapDispatchToProps = { //object form
+    createProduct: startCreatingProduct,
 
 };
 
