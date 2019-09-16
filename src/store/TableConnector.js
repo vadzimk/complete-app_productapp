6a8aad1e1c30abe5d0687868f1339b61df721a73
Vadzim <vadzimk@hotmail.com>
2019-09-16 11:38:38 -0700

    refactored TableConnector with mapDispatchToProps

--- src/store/TableConnector.js
+++ src/store/TableConnector.js
@@ -8,30 +8,46 @@ export const TableConnector = (dataType, presentationComponent) => {
 
     const mapStateToProps = (storeData, ownProps) => {
 
-        if(!ownProps.needSuppliers){
+        if (!ownProps.needSuppliers) {
             return {
                 products: storeData.modelData[T.PRODUCTS]
             }
         } else {
             return {
                 products: storeData.modelData[T.PRODUCTS],
-                suppliers: storeData.modelData[T.SUPPLIERS].map(supp=>(
+                suppliers: storeData.modelData[T.SUPPLIERS].map(supp => (
                     {
-                    ...supp,
-                        products: supp.products.map(id=>
+                        ...supp,
+                        products: supp.products.map(id =>
                             storeData.modelData[T.PRODUCTS]
-                                .find(p=>p.id===Number(id) || id)
-                                .map(val=>val.name || val)
+                                .find(p => p.id === Number(id) || id)
+                                .map(val => val.name || val)
                         )
-                }
+                    }
                 )),
             }
         }
     };
 
-    const mapDispatchToProps = {
-        editCallback: dataType === T.PRODUCTS ? startEditingProduct : startEditingSupplier,
-        deleteCallback: dataType === T.PRODUCTS ? deleteProduct : deleteSupplier,
+    // const mapDispatchToProps = {
+    //     editCallback: dataType === T.PRODUCTS ? startEditingProduct : startEditingSupplier,
+    //     deleteCallback: dataType === T.PRODUCTS ? deleteProduct : deleteSupplier,
+    // };
+
+
+    //function mapDispatchToProps:
+    const mapDispatchToProps = (dispatch, ownProps) => {
+        if(!ownProps.needSuppliers){
+            return {
+                editCallback: (...args)=>dispatch(startEditingProduct(...args)),
+                deleteCallback: (...args)=>dispatch(deleteProduct(...args))
+            }
+        } else {
+            return {
+                editCallback: (...args)=>dispatch(startEditingSupplier(...args)),
+                deleteCallback: (...args)=>dispatch(deleteSupplier(...args))
+            }
+        }
     };
 
     return connect(mapStateToProps, mapDispatchToProps)(presentationComponent);
